e284b2cbabe16fa9ee55442021f7ffbfee22d39a
tomibudis <tomibudis@gmail.com>
2022-02-03 01:36:42 +0700

    fix: revert config lint-stage

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,5 +3,5 @@ module.exports = {
     `next lint --fix --file ${filenames
       .map((file) => file.split(process.cwd())[1])
       .join(' --file ')}`,
-  // '**/*.{css,scss}': () => `npx stylelint **/*.{css,scss}`,
+  '**/*.{css,scss}': () => `npx stylelint **/*.{css,scss}`,
 }
